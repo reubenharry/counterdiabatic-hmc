e7e0548f5b1f6f98442a8c251e5d3cd694406069
= <reubenharry@gmail.com>
2025-11-09 23:57:03 -0500

    checkpoint: integrated blackjax

--- src/simulation.py
+++ src/simulation.py
@@ -13,9 +13,7 @@ import blackjax.smc.base as base
 import blackjax
 
 
-get_qs = lambda qp: qp[:, :qp.shape[1]//2]
-get_ps = lambda qp: qp[:, qp.shape[1]//2:]
-get_particles = lambda q, p: jnp.concatenate([q, p], axis=1)
+get_particles = lambda q, p: {'q': q, 'p': p}
 
 def initialize(M, make_T, make_V, key, dim, lam_fn, initial_sigma=1.0):
     snapshots = {'particles': [], 'weights': [], 'lam': [], 'resampling_events': [], 'times': [], 'weights_before_resampling': [], 'particles_before_resampling': []}
@@ -44,57 +42,37 @@ def initialize(M, make_T, make_V, key, dim, lam_fn, initial_sigma=1.0):
 
 
 
-def make_cdhmc_kernel(make_T, make_V, A_ansatz, time_next, lam_fn, dot_lam_fn):
+def make_cdhmc_kernel(make_T, make_V, A_ansatz, time_next, lam_fn, dot_lam_fn, integrator_type="leapfrog"):
+      
+    if A_ansatz is None:
+        step = jax.vmap(lambda q, p, lam, lam_next, dot_lam, dot_lam_next, delta_t: (make_leapfrog_step(make_T(lam), make_V(lam), make_T(lam_next), make_V(lam_next), lam_fn, dot_lam_fn))(q, p, delta_t), in_axes=(0, 0, None, None, None, None, None))       
+        # Choose integrator based on integrator_type
+    elif integrator_type == "leapfrog":
+            step = jax.vmap(lambda q, p, lam, lam_next, dot_lam, dot_lam_next, delta_t: (make_cd_leapfrog_step(make_T, make_V, A_ansatz, lam, lam_next, dot_lam, dot_lam_next, lam_fn, dot_lam_fn))(q=q, p=p, eps=delta_t), in_axes=(0, 0, None, None, None, None, None))
+    elif integrator_type == "implicit_midpoint":
+            step = jax.vmap(lambda q, p, lam, lam_next, dot_lam, dot_lam_next, delta_t: (make_cd_implicit_midpoint_step(make_T, make_V, A_ansatz, lam, lam_next, dot_lam, dot_lam_next))(q=q, p=p, eps=delta_t), in_axes=(0, 0, None, None, None, None, None))
+    else:
+            raise ValueError(f"Unknown integrator type: {integrator_type}. Must be 'leapfrog' or 'implicit_midpoint'")
 
-
-
-    # if A_ansatz is None:
-    #     step = lambda q, p, lam, lam_next, dot_lam, dot_lam_next, delta_t: make_leapfrog_step(make_T(lam), make_V(lam), make_T(lam_next), make_V(lam_next), lam_fn, dot_lam_fn)(q, p, delta_t)
-    step = make_cd_leapfrog_step
+            
+    lam_next = lam_fn(time_next)
+    dot_lam_next = dot_lam_fn(time_next)
 
     def cdhmc_kernel(state, key):
         time = state.update_parameters['time']
         lam = lam_fn(time)
-        lam_next = lam_fn(time_next)
         dot_lam = dot_lam_fn(time)
-        dot_lam_next = dot_lam_fn(time_next)
-
-        # q, p, log_weights = systematic_resample(get_qs(state.particles), get_ps(state.particles), jnp.log(state.weights), key, state.particles.shape[0])
-        # state = state._replace(particles=get_particles(q, p))
-
-        print("weights", state.weights)
-
-        # state = state._replace(weights=state.weights/jnp.sum(state.weights))
-
-
-        # new_qs, new_ps, new_log_weights = systematic_resample(get_qs(state.particles), get_ps(state.particles), jnp.log(state.weights), key, state.particles.shape[0])
-        # state = state._replace(particles=get_particles(new_qs, new_ps))
-        # state = state._replace(weights=jnp.exp(new_log_weights))
-
         delta_t = time_next - time
-        # delta_t = 0.2
-        def update_fn(k, p, u):
-            print(p.shape, "p.shape")
-            dim = p.shape[-1] // 2
-            qs, ps, _ = jax.vmap(lambda _, particle, update_parameters: (step(make_T, make_V, A_ansatz, lam, lam_next, dot_lam, dot_lam_next, lam_fn, dot_lam_fn))(q=particle[:dim], p=particle[dim:], eps=delta_t), in_axes=(0, 0, None))(k, p, u)
+    
+        def update_fn(k, particles, u):
+            qs, ps, _ = step(particles['q'], particles['p'], lam, lam_next, dot_lam, dot_lam_next, delta_t)
             return get_particles(qs, ps), None
+
         H = lambda lam: jax.vmap(lambda q, p: make_T(lam)(p) + make_V(lam)(q))
-
-        # def log_weights_fn(particle ) -> float:
-        #     return delta_t * (
-        #         -(H)(1.0)(particle) + (H)(0.0)(particle)
-        #     )
-        weight_fn = lambda old_particles, particles : H(lam)(get_qs(old_particles), get_ps(old_particles)) - H(lam_next)(get_qs(particles), get_ps(particles))
-        # indices = resampling.systematic(key, state.weights, state.particles.shape[0])
-        # particles = jax.tree.map(lambda x: x[indices], state.particles)
-        # state = state._replace(particles=particles)
+        weight_fn = lambda old_particles, particles : H(lam)(old_particles['q'], old_particles['p']) - H(lam_next)(particles['q'], particles['p'])
         new_state, info = blackjax.smc.base.step(key, state = state, update_fn = update_fn, weight_fn = weight_fn, resample_fn = resampling.systematic)
-
-        # new_state, info = blackjax_kernel(key, state)
-        # print(new_state.weights)
         return new_state, info
-        # raise Exception("ran blackjax kernel")
-    return cdhmc_kernel
+    return cdhmc_kernel, lam_next, dot_lam_next
 
 # =============================================================================
 # UNIFIED SIMULATION FUNCTION
@@ -117,9 +95,7 @@ def smc(
     use_weights=False, 
     ess_threshold=None, # 0.5 
     snapshot_every=1,
-    adaptive_step_size=False, 
     integrator_type="leapfrog", 
-    equilibration_steps=0,
     initial_sigma=1.0,
     resample_fn=systematic_resample,
     next_time=lambda t, k: t + 0.2,
@@ -153,42 +129,112 @@ def smc(
         For 'cd' type: (A_ansatz, snapshots, loss_histories, param_history)
     """
     
-    
-    
-    
-    
     snapshots, resampling_count, loss_histories, param_history, prev_H_vals, detailed_energy_stats, detailed_times, state = initialize(M, make_T, make_V, key, dim, lam_fn, initial_sigma)
     
-    
     for k in range(N_steps):
-
            
         lam_k = float(lam_fn(state.update_parameters['time']))
         dot_lam_k = float(dot_lam_fn(state.update_parameters['time']))
-        
-        # Check lambda values for NaNs
         if jnp.isnan(lam_k) or jnp.isnan(dot_lam_k): raise Exception()
-        
             
         # Handle ansatz fitting and loss calculation
         if A_ansatz is not None and (k % fit_every == 0):
             print(f"Fitting ansatz at step {k} with λ = {lam_k}")
 
-            samples = np.array(state.particles)
+            samples = jnp.concatenate([state.particles['q'], state.particles['p']], axis=1)
             check_nans("fitting_samples", samples, k)
-            weights = jnp.exp(normalize_log_weights(jnp.log(state.weights)))
             
             A_ansatz, loss_history = fit_gauge_potential(
                 lam_k, samples,
                 make_T=make_T, make_V=make_V, A_ansatz=A_ansatz,  # Pass current ansatz for warm start
                 num_iters=num_iters, lr=learning_rate,
-                use_regularization=False, weights=weights)
+                use_regularization=False, weights=state.weights)
             print(f"  Final loss: {loss_history[-1]:.6f}")
             if any(jnp.isnan(loss) for loss in loss_history):
                 print(f"⚠️  NaN detected in loss history at step {k}")
             
             loss_histories.append(loss_history)
         
+        t_k1 = next_time(state.update_parameters['time'], k)
+        
+        if k % momentum_refresh_interval == 0 and k > 0:
+            key, sub = jax.random.split(key)
+            p = jax.random.normal(sub, (M, dim))
+            state = state._replace(particles=get_particles(state.particles['q'], p))
+            
+            # T = make_T(lam_k)
+            # V = make_V(lam_k)
+            # prev_H_vals = jax.vmap(lambda qr, pr: T(pr) + V(qr))(get_qs(state.particles), get_ps(state.particles))
+
+        kernel, lam_next, dot_lam_next = make_cdhmc_kernel(make_T, make_V, A_ansatz, t_k1, lam_fn, dot_lam_fn)
+        key, sub = jax.random.split(key)
+        state, _ = kernel(state, sub)
+
+        if k % snapshot_every == 0: update_snapshots(snapshots, state, lam_next, t_k1)
+            
+
+        if check_nans(f"q", state.particles['q'], k) or check_nans(f"p", state.particles['p'], k) or check_nans(f"log_weights", jnp.log(state.weights), k): raise Exception(f"  Stopping simulation due to NaNs in HMC at step {k}")
+         
+        state = state._replace(update_parameters={'time': t_k1})        
+        if state.update_parameters['time'] > final_time:
+            break
+
+    return A_ansatz, snapshots, loss_histories, param_history, state
+
+
+def update_snapshots(snapshots, state, lam_next, t_k1):
+    snapshots['particles'].append(state.particles['q'])
+    snapshots['weights'].append(state.weights)
+    snapshots['lam'].append(float(lam_next))
+    snapshots['times'].append(t_k1)
+
+
+ # # 1. Run equilibration at current time (if enabled and taking snapshots)
+        # if equilibration_steps > 0 and k % snapshot_every == 0 and k < N_steps:
+        #     # Store pre-equilibration particles
+        #     pre_equil_q = np.array(q)
+        #     if 'pre_equilibration' in snapshots:
+        #         snapshots['pre_equilibration'].append(pre_equil_q)
+            
+        #     # Run equilibration steps using standard HMC at current lambda
+        #     # T_current = make_T(lam_k)
+        #     # V_current = make_V(lam_k)
+            
+        #     # Create HMC step function for equilibration
+        #     hmc_step = jax.vmap(lambda q, p, lam, lam_next, delta_t: (make_leapfrog_step(make_T(lam), make_V(lam), make_T(lam_next), make_V(lam_next), lam_fn, dot_lam_fn))(q, p, delta_t), in_axes=(0, 0, None, None, None))
+            
+        #     # Run equilibration steps
+        #     print("\n\nEquilibrating at time", t_k, "with lambda", lam_k)
+        #     key, equil_key, refresh_key = jax.random.split(key, 3)
+        #     p = jax.random.normal(refresh_key, (M, dim))
+            
+        #     for equil_iter in range(equilibration_steps):
+        #         # refresh_key, update_key = jax.random.split(equil_key)
+        #         # Refresh momentum before each equilibration step
+                
+        #         # Take HMC step
+        #         # subs = jax.random.split(update_key, M)
+        #         # print("\n\nlam_k\n\n", lam_k)
+        #         if k%5 == 0:
+        #             q, p, _ = jax.jit(hmc_step)(q, p, lam_k, lam_k, 1e-2)
+        #         if equil_iter%10 == 0:
+        #             equil_key, refresh_key = jax.random.split(equil_key)
+        #             p = jax.random.normal(refresh_key, (M, dim))
+            
+        #     # Store post-equilibration particles
+        #     post_equil_q = np.array(q)
+        #     if 'post_equilibration' in snapshots:
+        #         snapshots['post_equilibration'].append(post_equil_q)
+        
+        # 2. Save snapshot (after potential equilibration) - before evolution steps
+
+
+
+
+
+
+
+
         # Compute energy statistics at every timestep
         # stats = compute_energy_stats(get_qs(state.particles), get_ps(state.particles), lam_k, make_T, make_V, A_ansatz, jnp.log(state.weights))
         
@@ -251,137 +297,3 @@ def smc(
         
        
             
-        
-        # Record parameters for counterdiabatic simulations (do this before potential equilibration)
-
-        t_k1 = next_time(state.update_parameters['time'], k)
-        current_delta_t = t_k1 - state.update_parameters['time']
-        
-        # refresh momentum
-        if k % momentum_refresh_interval == 0 and k > 0:
-        # if True:
-            # old_energy = jax.vmap(lambda qr, pr: make_T(lam_k)(pr) + make_V(lam_k)(qr))(q, p)
-            # first resample
-            
-            # key, sub = jax.random.split(key)
-            # q, p, log_weights = resample_fn(state.particles['q'], state.particles['p'], jnp.log(state.weights), sub, M)
-            # state = state._replace(weights=jnp.exp(log_weights))
-            # state = state._replace(particles={'q': q, 'p': p})
-            key, sub = jax.random.split(key)
-            p = jax.random.normal(sub, (M, dim))
-            state = state._replace(particles=get_particles(get_qs(state.particles), p))
-            # new_energy = jax.vmap(lambda qr, pr: make_T(lam_k)(pr) + make_V(lam_k)(qr))(q, p)
-            # log_weights += old_energy - new_energy
-
-            T = make_T(lam_k)
-            V = make_V(lam_k)
-            prev_H_vals = jax.vmap(lambda qr, pr: T(pr) + V(qr))(get_qs(state.particles), get_ps(state.particles))
-
-             
-            
-        if True:
-            # # Use adaptive step size for time progression if enabled
-            # # step_delta_t = current_delta_t if simulation_type == 'cd' and adaptive_step_size else delta_t
-            # lam_k1 = float(lam_fn(t_k1))
-            # dot_lam_k1 = float(dot_lam_fn(t_k1))
-            
-            # if A_ansatz is None:
-            #     step = jax.vmap(lambda q, p, lam, lam_next, dot_lam, dot_lam_next, delta_t: (make_leapfrog_step(make_T(lam), make_V(lam), make_T(lam_next), make_V(lam_next), lam_fn, dot_lam_fn))(q, p, delta_t), in_axes=(0, 0, None, None, None, None, None))       
-            #     # Choose integrator based on integrator_type
-            # elif integrator_type == "leapfrog":
-            #         step = jax.vmap(lambda q, p, lam, lam_next, dot_lam, dot_lam_next, delta_t: (make_cd_leapfrog_step(make_T, make_V, A_ansatz, lam, lam_next, dot_lam, dot_lam_next, lam_fn, dot_lam_fn))(q=q, p=p, eps=delta_t), in_axes=(0, 0, None, None, None, None, None))
-            # elif integrator_type == "implicit_midpoint":
-            #         step = jax.vmap(lambda q, p, lam, lam_next, dot_lam, dot_lam_next, delta_t: (make_cd_implicit_midpoint_step(make_T, make_V, A_ansatz, lam, lam_next, dot_lam, dot_lam_next))(q=q, p=p, eps=delta_t), in_axes=(0, 0, None, None, None, None, None))
-            # else:
-            #         raise ValueError(f"Unknown integrator type: {integrator_type}. Must be 'leapfrog' or 'implicit_midpoint'")
-
-
-
-            # while True:
-            
-            # q_new, p_new, step_weights = jax.jit(step)(get_qs(state.particles), get_ps(state.particles), lam_k, lam_k1, dot_lam_k, dot_lam_k1, current_delta_t)
-
-            kernel = make_cdhmc_kernel(make_T, make_V, A_ansatz, t_k1, lam_fn, dot_lam_fn)
-            key, sub = jax.random.split(key)
-            state, _ = kernel(state, sub)
-
-            snapshots['particles'].append(np.array(get_qs(state.particles)))
-            snapshots['weights'].append(np.array(jnp.log(state.weights)))
-            snapshots['lam'].append(float(lam_fn(t_k1)))
-            snapshots['times'].append((t_k1))
-            
-            # if use_weights: 
-                
-            #     # Resample if ESS falls below threshold
-            #     # if ess_threshold is not None: 
-            #     if True:
-            #     # and ess < ess_threshold:
-            #         #  print(f"  Resampling at step {k} (ESS = {ess:.2f})")
-            #         if snapshot_every > 0 and k % snapshot_every == 0:
-            #             snapshots['weights_before_resampling'].append(np.array(jnp.log(state.weights)))
-            #             snapshots['particles_before_resampling'].append(np.array(get_qs(state.particles)))
-            #         q, p, log_weights = resample_fn(get_qs(state.particles), get_ps(state.particles), jnp.log(state.weights), key, M)
-            #         # state = state._replace(weights=jnp.exp(normalize_log_weights(log_weights)))
-            #         resampling_count += 1
-            #         # state = state._replace(particles=get_particles(q, p))
-            #         # Record the resampling event time
-            #         snapshots['resampling_events'].append(state.update_parameters['time'])
-
-        else:
-            kernel = make_cdhmc_kernel(make_T, make_V, A_ansatz, t_k1, lam_fn, dot_lam_fn)
-            key, sub = jax.random.split(key)
-            state, _ = kernel(state, sub)
-
-
-           
-         # Check for NaNs after step
-        if check_nans(f"q", get_qs(state.particles), k) or check_nans(f"p", get_ps(state.particles), k) or check_nans(f"log_weights", jnp.log(state.weights), k): raise Exception(f"  Stopping simulation due to NaNs in HMC at step {k}")
-         
-
-        state = state._replace(update_parameters={'time': next_time(state.update_parameters['time'], k)})
-        print(f"t_k: {state.update_parameters['time']}")
-        
-        if state.update_parameters['time'] > final_time:
-            break
-
-    return A_ansatz, snapshots, loss_histories, param_history, state
-
-
- # # 1. Run equilibration at current time (if enabled and taking snapshots)
-        # if equilibration_steps > 0 and k % snapshot_every == 0 and k < N_steps:
-        #     # Store pre-equilibration particles
-        #     pre_equil_q = np.array(q)
-        #     if 'pre_equilibration' in snapshots:
-        #         snapshots['pre_equilibration'].append(pre_equil_q)
-            
-        #     # Run equilibration steps using standard HMC at current lambda
-        #     # T_current = make_T(lam_k)
-        #     # V_current = make_V(lam_k)
-            
-        #     # Create HMC step function for equilibration
-        #     hmc_step = jax.vmap(lambda q, p, lam, lam_next, delta_t: (make_leapfrog_step(make_T(lam), make_V(lam), make_T(lam_next), make_V(lam_next), lam_fn, dot_lam_fn))(q, p, delta_t), in_axes=(0, 0, None, None, None))
-            
-        #     # Run equilibration steps
-        #     print("\n\nEquilibrating at time", t_k, "with lambda", lam_k)
-        #     key, equil_key, refresh_key = jax.random.split(key, 3)
-        #     p = jax.random.normal(refresh_key, (M, dim))
-            
-        #     for equil_iter in range(equilibration_steps):
-        #         # refresh_key, update_key = jax.random.split(equil_key)
-        #         # Refresh momentum before each equilibration step
-                
-        #         # Take HMC step
-        #         # subs = jax.random.split(update_key, M)
-        #         # print("\n\nlam_k\n\n", lam_k)
-        #         if k%5 == 0:
-        #             q, p, _ = jax.jit(hmc_step)(q, p, lam_k, lam_k, 1e-2)
-        #         if equil_iter%10 == 0:
-        #             equil_key, refresh_key = jax.random.split(equil_key)
-        #             p = jax.random.normal(refresh_key, (M, dim))
-            
-        #     # Store post-equilibration particles
-        #     post_equil_q = np.array(q)
-        #     if 'post_equilibration' in snapshots:
-        #         snapshots['post_equilibration'].append(post_equil_q)
-        
-        # 2. Save snapshot (after potential equilibration) - before evolution steps
